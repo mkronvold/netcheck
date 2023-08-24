1bc304597588e35e79ad75549bd2a21896ac9051
mkronvold <>
2023-08-24 13:00:37 -0500

    added --no-service options and root check

--- install.sh
+++ install.sh
@@ -5,6 +5,17 @@
 INSTALLDIR=$HOME/.local/bin
 LOGDIR=$HOME/.netcheck
 
+if [ "${1}" == "--no-service" ]; then
+  SERVICE=
+  echo "Not installing service by-request"
+elif [ "$(whoami)" == "root" ]; then
+  SERVICE=1
+  echo "Running as sudo/root, installing service"
+else
+  SERVICE=
+  echo "Not running as sudo/root, can not install service"
+fi
+
 # install
 mkdir -p $INSTALLDIR
 cp netcheck.sh $INSTALLDIR
@@ -21,22 +32,24 @@ cp internet_status_chart.sh $INSTALLDIR
 mkdir -p $LOGDIR
 cp -rv log/* $LOGDIR/
 
-# service files
-sudo cp -rv etc/default /
-sudo cp -rv etc/init.d /
 
-# quote paths for sed'sake
-INSTALLDIRQUOTED=$(printf '%s' "$INSTALLDIR" | sed 's/[#\]/\\\0/g')
-LOGDIRQUOTED=$(printf '%s' "$LOGDIR" | sed 's/[#\]/\\\0/g')
+if [ $SERVICE ]; then
+  # service files
+  sudo cp -rv etc/default /
+  sudo cp -rv etc/init.d /
 
+  # quote paths for sed'sake
+  INSTALLDIRQUOTED=$(printf '%s' "$INSTALLDIR" | sed 's/[#\]/\\\0/g')
+  LOGDIRQUOTED=$(printf '%s' "$LOGDIR" | sed 's/[#\]/\\\0/g')
 
-sed -e "s#INSTALLDIR#${INSTALLDIRQUOTED}#g" -e "s#LOGDIR#${LOGDIRQUOTED}#g" etc/sudoers.d/netcheck | sudo tee /etc/sudoers.d/netcheck > /dev/null
-sed -e "s#INSTALLDIR#${INSTALLDIRQUOTED}#g" -e "s#LOGDIR#${LOGDIRQUOTED}#g" etc/default/netcheck | sudo tee /etc/default/netcheck > /dev/null
+  sed -e "s#INSTALLDIR#${INSTALLDIRQUOTED}#g" -e "s#LOGDIR#${LOGDIRQUOTED}#g" etc/sudoers.d/netcheck | sudo tee /etc/sudoers.d/netcheck > /dev/null
+  sed -e "s#INSTALLDIR#${INSTALLDIRQUOTED}#g" -e "s#LOGDIR#${LOGDIRQUOTED}#g" etc/default/netcheck | sudo tee /etc/default/netcheck > /dev/null
 
-# creating python link if it's missing
-[ $(which python) ] || sudo ln -s $(which python3) /usr/bin/python
+  # creating python link if it's missing
+  [ $(which python) ] || sudo ln -s $(which python3) /usr/bin/python
 
-# Verifying service installed correctly
-sudo service netcheck status
+  # Verifying service installed correctly
+  sudo service netcheck status
 
-echo add \"service netcheck start\" to /etc/wsl.conf
+  echo add \"service netcheck start\" to /etc/wsl.conf
+fi
